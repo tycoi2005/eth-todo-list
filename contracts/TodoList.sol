fef55c090503062e1c987acb52f631f4d0e539f1
Jaken Ty <toanhdpy@gmail.com>
2023-01-09 11:30:32 +0700

    create Task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,6 +2,22 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract  TodoList {
-    uint taskCount = 0;
+   uint public taskCount = 0;
 
+   struct Task {
+    uint id;
+    string content;
+    bool completed;
+   }
+
+   mapping (uint => Task) public tasks;
+
+   function createTask (string memory content) public{
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, content, false);
+   }
+
+   constructor() public {
+    createTask("TodoList first task");
+   }
 }
